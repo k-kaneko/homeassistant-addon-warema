3593d5d88e112814f6b8b106a5d0326f20fe29e4
k-kaneko <kaneko@dmc.keio.ac.jp>
2025-07-13 21:12:44 +0900

    Update bridge.js

--- warema-bridge/rootfs/srv/bridge.js
+++ warema-bridge/rootfs/srv/bridge.js
@@ -153,7 +153,7 @@ function callback(err, msg) {
         stickUsb.setPosUpdInterval(30000);
         break
       case 'wms-vb-rcv-weather-broadcast':
-//          if ( 0 ) {
+//        if ( 0 ) {
         if (registered_shades.includes(msg.payload.weather.snr)) {
           client.publish('warema/' + msg.payload.weather.snr + '/illuminance/state', msg.payload.weather.lumen.toString())
           client.publish('warema/' + msg.payload.weather.snr + '/temperature/state', msg.payload.weather.temp.toString())
